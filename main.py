a7ee3f7b731898ec9f51462b1b09637f911b04d0
Julio Cesar <julio0033@hotmail.com>
2023-05-01 21:35:13 -0300

    Criação da ferramenta balde para preencher o desenho

--- main.py
+++ main.py
@@ -46,19 +46,50 @@ def get_row_col_from_pos(pos):
 
     return row, col
 
+def flood_fill_bfs(screen_array, start_pos, fill_color):
+    x, y = start_pos
+    start_color = screen_array[x][y]
+    q = Queue()
+    q.put(start_pos)
+    visited = set()
+
+    while not q.empty():
+        x, y = q.get()
+
+        # If the current position is already visited or out of screen boundary, skip
+        if (x, y) in visited or x < 0 or x >= ROWS or y < 0 or y >= COLS:
+            continue
+
+        # If the current position has the same color as the starting position, fill it with the fill color
+        if screen_array[x][y] == start_color:
+            screen_array[x][y] = fill_color
+
+            # Add the neighboring positions to the queue
+            q.put((x-1, y))
+            q.put((x+1, y))
+            q.put((x, y-1))
+            q.put((x, y+1))
+
+            # Draw the filled screen
+            pygame.draw.rect(WIN, screen_array[x][y], (x, y, PIXEL_SIZE, PIXEL_SIZE))
+
+        visited.add((x, y))
+
 run = True
 clock = pygame.time.Clock()
 grid = init_grid(ROWS, COLS, BG_COLOR)
 drawing_color = BLACK
 button_y = HEIGHT - TOOLBAR_HEIGHT/2 - 25
 buttons = [
-    Button(10, button_y, 50, 50, WHITE, "Erase"),
-    Button(70, button_y, 50, 50, WHITE, "Clear"),
+    Button(10, button_y, 50, 50, WHITE, "Fill"),
+    Button(70, button_y, 50, 50, WHITE, "Erase"),
+    Button(130, button_y, 50, 50, WHITE, "Clear"),
     Button(360, button_y, 50, 50, BLACK),
     Button(420, button_y, 50, 50, RED),
     Button(480, button_y, 50, 50, GREEN),
     Button(540, button_y, 50, 50, BLUE)
 ]
+fill = False
 
 while(run):
     clock.tick(FPS)
@@ -87,6 +118,19 @@ while(run):
                     
                     elif button.text == "Erase":
                         drawing_color = WHITE
+
+                    elif button.text == "Fill":
+                        fill = True
+
+                        while (fill):
+                            for event in pygame.event.get():
+                                if pygame.mouse.get_pressed()[0]:
+                                    try:
+                                        pos = get_row_col_from_pos(pygame.mouse.get_pos())
+                                        flood_fill_bfs(grid, pos, drawing_color)
+                                        fill = False
+                                    except IndexError:
+                                        pass
 
     draw(WIN, grid, buttons)
 
